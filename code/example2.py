f8fa1a1c3b25a133495be9e1c945f4b83eb386d7
Pimin Konstantin Kefaloukos <skipperkongen@gmail.com>
2013-05-27 15:56:27 +0200

    no commit message

--- code/example2.py
+++ code/example2.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
 		'zoomlevels': 15,
 		'rank_by': 'ST_Length(wkb_geometry)',
 	 	'partition_by' : 'type',
-		'_maxdensity': 0.75,
+		'_maxdensity': 0.25,
 		'simplify': True
 	}
 	cm = CvlMain(HittingSetHeuristic(**query), [density.DensityConstraint(**query), allornothing.AllOrNothingConstraint(**query)], **query)

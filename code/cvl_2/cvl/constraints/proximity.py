f39aed459d4630a794f79957e5141f958e421581
Pimin Konstantin Kefaloukos <skipperkongen@gmail.com>
2013-05-28 16:09:22 +0200

    no commit message

---
+++ code/cvl_2/cvl/constraints/proximity.py
@@ -0,0 +1,55 @@
+SET_UP = \
+""""""
+
+FIND_CONFLICTS = \
+"""
+SELECT 
+	ROW_NUMBER() OVER (ORDER BY 1) AS conflict_id, 
+	unnest(array[l.{id}, r.{id}]) AS record_id, 
+	unnest(array[l._rank, r._rank]) as record_rank,
+	1 as min_hits
+FROM 
+	{table} l 
+JOIN
+	{table} r
+ON 
+	l.{id} < r.{id}
+AND	l._partition = r._partition
+AND	l._tile_level = {current_z}
+AND	r._tile_level = {current_z}
+AND	ST_DWithin(l.{geometry}, r.{geometry}, ST_ResZ({current_z}, 256) * {_pixels});
+"""
+
+CLEAN_UP = \
+""""""
+
+class ProximityConstraint(object):
+	"""Implementation of constraint 'proximity'"""
+	def __init__(self, **query):
+		super(ProximityConstraint, self).__init__()
+		self.query = query
+		# cast _pixels to integer and set default if missing
+		self.query['_pixels'] = int(self.query.get('_pixels', 5))
+
+	
+	def set_up(self, current_z):
+		params = dict(self.query.items() + [('current_z', current_z)])
+		return [SET_UP.format(**params)]
+
+	def find_conflicts(self, current_z):
+		params = dict(self.query.items() + [('current_z', current_z)])
+		return [FIND_CONFLICTS.format(**params)]
+	
+	def clean_up(self, current_z):
+		params = dict(self.query.items() + [('current_z', current_z)])
+		return [CLEAN_UP.format(**params)]
+
+if __name__ == '__main__':
+	query = {'table': 'us_airports_output','geometry': 'wkb_geometr', 'id': 'ogc_fid', '_pixels': 3}
+	cb = ProximityConstraint(**query)
+	
+	code = []
+	code.extend(cb.set_up(current_z = 15))
+	code.extend(cb.find_conflicts(current_z = 15))
+	code.extend(cb.clean_up(current_z = 15))
+	print "".join(code)
---
+++ code/cvl_2/cvl/utility/meter_per_pixel.py
@@ -0,0 +1,9 @@
+"""Meter per pixel (EPSG:3857): Function that returns number of meters per pixel for different zoom-levels (z) in EPSG:3857 (WebMercator). Default tile-size is 256x256."""
+def meter_per_pixel_3857(z, tilesize=256.0):
+	assert z >= 0
+	level0_meter = 20037508.34 * 2
+	return (level0_meter / 2**z) / tilesize
+
+meter_per_pixel_3857(3)
+# returns: 19567.87923828125
+

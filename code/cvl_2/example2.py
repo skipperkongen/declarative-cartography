01f93cced33e2271e5a00cefa17a2a54f40b80ac
Pimin Konstantin Kefaloukos <skipperkongen@gmail.com>
2013-06-04 15:16:56 +0200

    no commit message

---
+++ code/cvl_2/example2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from cvl.compiler import CvlCompiler
+from cvl.query import Query, WILDCARD
+
+if __name__ == '__main__':
+	query_dict = {
+		'zoomlevels': 20,
+		'input': 'mini_highway',
+		'output': 'mini_highway_output',
+		'fid': 'ogc_fid',
+		'geometry': 'wkb_geometry',
+		'other': ['type', 'name', 'oneway', 'lanes'],
+		'rank_by': '1',
+	 	'partition_by' : 'type',
+		'merge_partitions': [
+			(['motorway','motorway_link'], 'motorways'),
+			(['primary','primary_link','secondary','secondary_link','tertiary','tertiary_link','road'], 'big_streets'),
+			(['residential','pedestrian','living_street'], 'medimum_streets'),
+			(WILDCARD, 'the_rest')
+		],
+		'subject_to' : [('avgdensity', 40)],
+		'force_level': [('the_rest', 20)],
+		'transform_by': ['simplify_once']
+	}
+	query = Query(**query_dict)
+	comp = CvlCompiler(query)
+	print comp.generate_sql()

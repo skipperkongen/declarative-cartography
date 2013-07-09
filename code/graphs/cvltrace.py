278bf7037cc2b678f557741e175092940d4e24f4
Pimin Konstantin Kefaloukos <skipperkongen@gmail.com>
2013-07-09 11:33:22 +0200

    Implemented trace parser with changes

--- code/graphs/cvltrace.py
+++ code/graphs/cvltrace.py
@@ -1,36 +1,21 @@
 __author__ = 'kostas'
 from datetime import datetime
-import re
-
 
 class TraceReader(object):
 
     def __init__(self, trace_path):
         with open(trace_path, 'r') as f:
-            traces = []
-            current_trace = None
+            self.traces = []
+            self.current_trace = None
             for line in f:
                 line = line.strip()
-                event = self._parse_line(line)
-                if event['event'] == 'BEGIN_TRANSACTION':
-                    current_trace = Trace(ts_begin=event['timestamp'], trace_name=event['jobname'])
-                elif event['event'] == 'COMMIT':
-                    current_trace.end(event['timestamp'])
-                    traces.append(current_trace)
-                    current_trace = None
-                else:
-                    current_trace.add_event(event)
-        self._traces = traces
+                event_dict = self.get_event_dictionary(line)
+                self.handle_event(event_dict)
 
     def get_traces(self):
-        return self._traces
+        return self.traces
 
-    def _parse_line(self, line):
-        # example:
-        # 01/07/2013 11:11:54.207973 [7k_pt_airports_cellbound16_bound] found_conflicts 12
-        # fields[0]  fields[1]       fields[2]                          fields[3]       fields[4:]
-
-        # strftime("%d/%m/%Y %H:%M:%S.%f")
+    def get_event_dictionary(self, line):
         fields = line.split(" ")
         date_str = "{0:s} {1:s}".format(fields[0], fields[1])
         value = None if len(fields) <= 4 else eval(" ".join(fields[4:]))
@@ -41,6 +26,23 @@ class TraceReader(object):
             'value': value
         }
 
+    def handle_event(self, event):
+        """
+        Three cases:
+            BEGIN TRANSACTION: create new trace object
+            COMMIT: append current trace object to traces list
+            OTHER: add information to current trace (handled by trace object)
+        """
+        if event['event'] == 'BEGIN_TRANSACTION':
+            self.current_trace = Trace(ts_begin=event['timestamp'], trace_name=event['jobname'])
+        elif event['event'] == 'COMMIT':
+            self.current_trace.end(event['timestamp'])
+            self.traces.append(self.current_trace)
+            self.current_trace = None
+        else:
+            # HANDLE OTHER EVENT
+            self.current_trace.add_event(event)
+
 
 class Trace(object):
 
@@ -50,73 +52,56 @@ class Trace(object):
         self.ts_end = None
         # extract solver and constraint from jobname
         self.name = trace_name
-        match = re.search('(bound|lp|heuristic)', trace_name)
-        self.solver = match.group(1) if match else None
-        match = re.search('(proximity[0-9]+|cellbound[0-9]+)', trace_name)
-        self.constraint = match.groups() if match else None
         # private stuff
-        self._init_info = {'operations': {}}
-        self._levels = {}
-        self._stats = {}
+        self.initialization = None
+        self.solver = None
+        self.constraints = None
+        self.inputstats = None
+        self.levels = []
+        self.current_level = None
 
     def add_event(self, event):
 
         time_passed = event['timestamp'] - self._last_ts
+        event_type = event['event']
+        print type(event['value']), event_type
 
-        if event['event'] == 'INITIALIZED':
-            self._init_info['operations']['initialize'] = time_passed
+        if event_type == 'INITIALIZED':
+            self.initialization = time_passed
 
-        elif event['event'] == 'MERGED_PARTITIONS':
-            self._init_info['operations']['merge_partitions'] = time_passed
+        elif event_type == 'SOLVER':
+            self.solver = event['value']
 
-        elif event['event'] == 'INITIALIZED_LEVEL':
-            # begin new level
-            self._current_level = {
-                'begin': self._last_ts,
-                'num_records': 0,
-                'agg_rank': 0,
-                'operations': {}
+        elif event_type == 'CONSTRAINTS':
+            self.constraints = event['value']
+
+        elif event_type == 'INPUTSTATS':
+            self.inputstats = event['value']
+
+        elif event_type == 'INITIALIZED_LEVEL':
+            self.current_level = {
+                'zoom': event['value'],
+                'timing': {'initialized': time_passed}
             }
-            self._current_level['operations']['initialize_level'] = time_passed
-            zoom = event['value']
-            self._levels[zoom] = self._current_level
 
-        elif event['event'] == 'FINALIZED_LEVEL':
-            self._current_level['duration'] = event['timestamp'] - self._current_level['begin']
-            del self._current_level['begin']
-            self._current_level['operations']['finalize_level'] = time_passed
+        elif event_type == 'FOUND_CONFLICTS':
+            self.current_level['timing']['found_conflicts'] = time_passed
 
-        elif event['event'] == 'FORCED_LEVEL':
-            self._current_level['operations']['force_level'] = time_passed
+        elif event_type == 'RESOLVED_CONFLICTS':
+            self.current_level['timing']['resolved_conflicts'] = time_passed
 
-        elif event['event'] == 'FOUND_CONFLICTS':
-            self._current_level['operations']['find_conflicts'] = time_passed
+        elif event_type == 'LEVELSTATS':
+            self.current_level['stats'] = event['value']
 
-        elif event['event'] == 'RESOLVED_CONFLICTS':
-            self._current_level['operations']['resolve_conflicts'] = time_passed
-
-        elif event['event'] == 'TRANSFORMED_LEVEL':
-            self._current_level['operations']['transform_level'] = time_passed
-
-        elif event['event'] == 'STATS':
-            self._stats[event['value']['cvl_zoom']] = event['value']
-
-        elif event['event'] == 'STATS2':
-            self.num_records = event['value']['total_recs']
-            self.total_rank = event['value']['total_rank']
+        elif event_type == 'FINALIZED_LEVEL':
+            self.levels.append(self.current_level)
 
         else:
-            raise Exception('unhandled event: {0:s}'.format(event['event']))
+            raise Exception('unhandled event: {0:s}'.format(event_type))
 
         self._last_ts = event['timestamp']
 
     def end(self, ts_end):
         self.ts_end = ts_end
         self.duration = self.ts_end - self.ts_begin
-        # update levels
-        records_remain = self.num_records
-        rank_remain = self.total_rank
-        for foo in bar:
-            records_remain -= self._stats[i]
-            pass
 
---
+++ code/make_graph.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "------------"
+echo "INSTRUCTIONS"
+echo "------------"
+echo "This script only prints this message. To create a graph, use the script ./graphs/cvlgraph.py"
+echo ""
+./graphs/cvlgraph.py --help
+echo ""
+echo "Example: ./graphs/cvlgraph.py -g stack ../results/traces/macbook/7k_pt_airports.py.log"
+echo ""
+echo "thank you"

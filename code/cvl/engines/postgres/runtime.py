6e75560791d94b7203f86af4f48b8a4b8dcbcafa
Pimin Konstantin Kefaloukos <skipperkongen@gmail.com>
2013-07-12 12:11:19 +0200

    Swapped implementation for ST_CellsForPolygon

--- code/cvl/engines/postgres/runtime.py
+++ code/cvl/engines/postgres/runtime.py
@@ -139,41 +139,6 @@ ADD_RUNTIME = \
       );
     $$ LANGUAGE sql IMMUTABLE STRICT;
 
-    CREATE OR REPLACE FUNCTION CVL_CellsForPolygon
-    (
-      geom geometry,
-      cell_size double precision,
-      x0 double precision DEFAULT -20037508.34,
-      y0 double precision DEFAULT -20037508.34,
-      OUT pt geometry
-    ) RETURNS SETOF geometry AS
-    $$
-    SELECT
-        ST_SnapToGrid(PT.pt, $3 + $2/2, $4 + $2/2, $2, $2)
-    FROM
-    (
-        SELECT
-            ST_SetSrid( ST_Point(ST_XMin($1) + i*$2, ST_YMin($1) + j*$2), ST_Srid($1)) AS pt
-        FROM
-            generate_series(0, (ceil(ST_XMax( $1 ) - ST_Xmin( $1 )) / $2)::integer) AS i,
-            generate_series(0, (ceil(ST_YMax( $1 ) - ST_Ymin( $1 )) / $2)::integer) AS j
-    ) PT, (SELECT ST_AsRaster(
-                $1,
-                $2,
-                $2,
-                $3,
-                $4,
-                '1BB',
-                1::double precision,
-                0::double precision,
-                0::double precision,
-                0::double precision,
-                true
-            ) as raster) RASTER
-    WHERE
-        ST_Value(RASTER.raster, PT.pt, false) = 1
-    $$ LANGUAGE sql IMMUTABLE STRICT;
-
 
     CREATE OR REPLACE FUNCTION CVL_RasterToPoints
     (
@@ -206,7 +171,7 @@ ADD_RUNTIME = \
     $$ LANGUAGE sql IMMUTABLE STRICT;
 
 
-    CREATE OR REPLACE FUNCTION CVL_CellsForPolygon2
+    CREATE OR REPLACE FUNCTION CVL_CellsForPolygon
     (
       geom geometry,
       cell_size double precision,

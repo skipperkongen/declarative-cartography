bb4f8b2852f20a720868c38b9acb7b498363d29f
Pimin Konstantin Kefaloukos <skipperkongen@gmail.com>
2013-07-21 18:22:15 +0200

    *sigh redoing scalability... changed number of zoom-levels to 10.

--- code/test_scalability_pnt.py
+++ code/test_scalability_pnt.py
@@ -12,22 +12,25 @@ if __name__ == '__main__':
 
     dataset = DATASETS['fractal']
     while current_size <= dataset['size']*2:
-        for constraint_name in ['A', 'B']:
-            constraint = CONSTRAINTS[constraint_name]
-            job_name = "scalapnt_fractal_{0:d}_{1:s}_heuristic".format(
-                current_size,
-                constraint[0][0],
-            )
-            QUERY_DICT['input'] = dataset['input'].format(current_size)
-            QUERY_DICT['subject_to'] = constraint
-            QUERY_DICT['rank_by'] = dataset['rank_by']
-            query = Query(**QUERY_DICT)
-            print compiler.compile(
-                query,
-                solver='heuristic',
-                target='postgres',
-                log_file='/tmp/cvl.log',
-                job_name=job_name,
-                analytics=False
-            )
+        for solver in ['heuristic','lp']:
+            for constraint_name in ['A', 'B']:
+                constraint = CONSTRAINTS[constraint_name]
+                job_name = "scalapnt_fractal_{0:d}_{1:s}_{2:s}".format(
+                    current_size,
+                    constraint[0][0],
+                    solver
+                )
+                QUERY_DICT['input'] = dataset['input'].format(current_size)
+                QUERY_DICT['subject_to'] = constraint
+                QUERY_DICT['rank_by'] = dataset['rank_by']
+                query = Query(**QUERY_DICT)
+                query.zoomlevels = 10
+                print compiler.compile(
+                    query,
+                    solver=solver,
+                    target='postgres',
+                    log_file='/tmp/cvl.log',
+                    job_name=job_name,
+                    analytics=False
+                )
         current_size *= 2

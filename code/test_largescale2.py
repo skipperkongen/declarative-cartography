a0cddb4984a1701d067eb3e08a99628bf0004dad
Pimin Konstantin Kefaloukos <skipperkongen@gmail.com>
2013-07-21 12:49:00 +0200

    Trying new large scale test with proximity constraint. test_largescale.py did not work for some reason.

---
+++ code/test_largescale2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+__author__ = 'kostas'
+
+from test_util import *
+from cvl.compiler import CvlCompiler
+from cvl.framework.query import Query
+
+if __name__ == '__main__':
+    compiler = CvlCompiler()
+
+    dataset = DATASETS['fractal']
+    constraint = CONSTRAINTS['B']
+
+    job_name = "xl_fractal_heuristic_B"
+    QUERY_DICT['input'] = dataset['input'].format(dataset['size'])
+    QUERY_DICT['subject_to'] = constraint
+    QUERY_DICT['rank_by'] = dataset['rank_by']
+    query = Query(**QUERY_DICT)
+    print compiler.compile(
+        query,
+        solver='heuristic',
+        target='postgres',
+        log_file='/tmp/cvl.log',
+        job_name=job_name,
+        analytics=False
+    )
+

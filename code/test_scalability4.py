529c3a0404c3fbeff07c583bb6271b3a4d108bdf
Pimin Konstantin Kefaloukos <skipperkongen@gmail.com>
2013-07-21 16:40:36 +0200

    Bloody hell, there was an error in the us_waterways test.

---
+++ code/test_scalability4.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+__author__ = 'kostas'
+
+from test_util import *
+from cvl.compiler import CvlCompiler
+from cvl.framework.query import Query
+
+if __name__ == '__main__':
+    compiler = CvlCompiler()
+
+    MAX = 3000000
+    current_size = 5000
+
+    while current_size < MAX:
+        dataset = DATASETS['usriversandstreams']
+        for constraint_name in ['A', 'B']:
+            constraint = CONSTRAINTS[constraint_name]
+            for solver in ['heuristic']:
+                job_name = "scala3_{0:s}_{1:d}_{2:s}_{3:s}".format(
+                    'usriversandstreams',
+                    current_size,
+                    constraint[0][0],
+                    solver
+                )
+                QUERY_DICT['input'] = dataset['input'].format(current_size)
+                QUERY_DICT['subject_to'] = constraint
+                QUERY_DICT['rank_by'] = dataset['rank_by']
+                query = Query(**QUERY_DICT)
+                print compiler.compile(
+                    query,
+                    solver=solver,
+                    target='postgres',
+                    log_file='/tmp/cvl.log',
+                    job_name=job_name,
+                    analytics=False
+                )
+        current_size *= 2

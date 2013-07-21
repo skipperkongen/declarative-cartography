94663ece4053af282b5ba24e8324e2efe4d664fb
Pimin Konstantin Kefaloukos <skipperkongen@gmail.com>
2013-07-22 00:47:16 +0200

    LPGA + Proximity + Points does not scale, see code/graphs/cvlgraph.py -g print_nostat results/scraps/scale_pnt_progress.log

--- code/test_scalability_pnt.py
+++ code/test_scalability_pnt.py
@@ -14,6 +14,8 @@ if __name__ == '__main__':
     while current_size <= dataset['size']*2:
         for solver in ['heuristic','lp']:
             for constraint_name in ['A', 'B']:
+                if solver == 'lp' and constraint_name == 'B':
+                    continue
                 constraint = CONSTRAINTS[constraint_name]
                 job_name = "scalapnt_fractal_{0:d}_{1:s}_{2:s}".format(
                     current_size,

b67c684d091fa000786a12e25e5c98ad0c230a6f
Pimin Konstantin Kefaloukos <skipperkongen@gmail.com>
2013-07-22 21:57:19 +0200

    change to AVERAGE optimality ratio

--- code/graphs/cvlgraph.py
+++ code/graphs/cvlgraph.py
@@ -21,16 +21,37 @@ __author__ = 'kostas'
 
 class Scalability(object):
 
-    def __init__(self, traces, table_filter=None):
-        if table_filter:
-            self.traces = filter(lambda x: x.input_table == table_filter, traces)
-            print "using filter: {0:s}".format(table_filter)
-        else:
-            self.traces = traces
-            print "no filter"
+    def __init__(self, traces):
+        self.traces = traces
+        self.ds_name = [trace.input_table for trace in traces][0]
 
     def write_out(self):
-        # traces group by (table, constraint, solver)
+        to_label = lambda key: \
+            ('SGA' if key[0][0] == 'h' else 'LPGA') + (' + A' if key[1][0] == 'c' else ' + B')
+
+        x_formatter = lambda x, pos: 'foo'
+        # series for group by (table, constraint, solver)
+        sizes = set()
+        series = {}
+        for trace in self.traces:
+            key = (trace.solver,trace.constraints)
+            series.setdefault(key, {})[trace.inputstats['total_recs']] = trace.duration
+
+        for key, serie in series.items():
+            sizes = sizes.union([size for size in sorted(serie)])
+            x = [size for size in sorted(serie)]
+            y = [serie[size].total_seconds() for size in sorted(serie)]
+            plt.loglog(x, y, 'o-', label=to_label(key))
+        leg = plt.legend(loc='best',fancybox=True)
+        leg.get_frame().set_alpha(0.6)
+        plt.xlabel('Data size')
+        plt.ylabel('Runtime (seconds)')
+        filename = os.path.join(output_dir, "scal_{0:s}.png".format(self.ds_name))
+        plt.subplots_adjust(bottom=0.15)
+        plt.savefig(filename)
+        plt.clf()
+        print "Writing figure to {0:s}".format(filename)
+        #plt.show()
 
 
 class Stack(object):
@@ -39,6 +60,9 @@ class Stack(object):
         self.trace = trace
 
     def write_out(self):
+        fig = plt.figure()
+        ax1 = fig.add_subplot(111)
+
         width = .8
         zoom_levels = list(reversed(range(len(trace.levels))))
         dtype = [('tasks', 'S16')] + [('', np.float32)]*len(zoom_levels)
@@ -64,14 +88,14 @@ class Stack(object):
         tasks = y['tasks']
         bottom = np.zeros(len(zoom_levels))
         for i in range(len(data)):
-            bt = plt.bar(range(len(data[i])), data[i], width=width,
+            bt = ax1.bar(range(len(data[i])), data[i], width=width,
                          color=cm.hsv(24*i), label=tasks[i],
                          bottom=bottom)
             bottom += data[i]
         plt.xticks(np.arange(len(zoom_levels)) + (width/2),
                    [int(zoom) for zoom in zoom_levels])
-        plt.xlabel('Zoom')
-        plt.ylabel('Runtime (seconds)')
+        ax1.set_xlabel('Zoom')
+        ax1.set_ylabel('Runtime (seconds)')
         if len(self.trace.constraints) == 30:
             constr = 'AB'
         elif self.trace.constraints.startswith('cell'):
@@ -82,10 +106,18 @@ class Stack(object):
         #    self.trace.input_table,
         #    self.trace.solver,
         #    constr))
+        ax2 = ax1.twinx()
+
+        ax2.plot(range(len(zoom_levels)), [level['levelstats']['num_c'] for level in trace.levels], 'o-', linewidth=2,
+                 c="black")
+        ax2.set_ylabel('conflicts')
+
         if trace.input_table == 'pnt_7k_airports':
-            leg = plt.legend(loc='upper left', fancybox=True)
+            leg = ax1.legend(loc='upper left', fancybox=True)
         else:
-            leg = plt.legend(loc='upper right',fancybox=True)
+            leg = ax1.legend(loc='upper right',fancybox=True)
+
+
         leg.get_frame().set_alpha(0.6)
         #plt.gca().yaxis.set_major_formatter(FuncFormatter(billions))
 
@@ -95,6 +127,7 @@ class Stack(object):
             self.trace.solver,
             constr))
         print "Writing figure to {0:s}".format(filename)
+        plt.subplots_adjust(wspace=1.4)
         plt.savefig(filename)
         plt.clf()
 
@@ -104,10 +137,10 @@ class OptimalityRatio(object):
     def __init__(self, trace):
         self.solution = 0
         self.lowerbound = 0
+        self.numlevels = len(trace.levels)
         for level in trace.levels:
-            zoom = level['zoom'] + 1
-            self.solution += (zoom * level['levelstats']['rank_lost'])
-            self.lowerbound += (zoom * level['levelstats']['lp_bound'])
+            self.solution += (level['levelstats']['rank_lost'])
+            self.lowerbound += (level['levelstats']['lp_bound'])
 
     def get_lowerbound(self):
         return self.lowerbound
@@ -116,7 +149,7 @@ class OptimalityRatio(object):
         return self.solution
 
     def get_ratio(self):
-        return self.solution / float(self.lowerbound)
+        return (self.solution / float(self.numlevels)) / (float(self.lowerbound) / self.numlevels)
 
 
 if __name__ == '__main__':
@@ -124,8 +157,6 @@ if __name__ == '__main__':
     parser = OptionParser(usage=usage)
     parser.add_option("-g", "--graph", default="print",
                       help="Type of graph to produce: stack(default), scalability")
-    parser.add_option("-t", "--table", default=None,
-                      help="Only do graph for this table")
 
     (options, args) = parser.parse_args()
     if len(args) == 0:
@@ -163,7 +194,7 @@ if __name__ == '__main__':
             chart.write_out()
 
     elif options.graph == 'scala':
-        chart = Scalability(tr.get_traces(), options.table)
+        chart = Scalability(tr.get_traces())
         chart.write_out()
     else:
         print 'unknown chart type'
